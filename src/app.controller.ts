cee050492a5f199ea7dd16fdb9930ed2b6b8494e
Luiz Otavio <luiz123o@hotmail.com>
2022-12-13 19:12:30 -0300

    :tada: start project

---
+++ src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './services/prisma.service';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  getHello() {
+    return this.prisma.notification.findMany();
+  }
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { category, content, recipientId } = body;
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { PrismaService } from './services/prisma.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [PrismaService],
+})
+export class AppModule {}
